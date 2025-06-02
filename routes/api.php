afcd7b4a3e1829da19641a9538f6f8803fbd43dc
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2025-06-02 12:17:16 +0800

    Update endpoint updateavatar

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::delete('/delete-gallery/{id}', [ProductsController::class, 'deleteGallery
 Route::put('/update-status-order/{id}', [OrdersController::class, 'updateStatusOrder'])->middleware('auth:sanctum');
 Route::post('/xendit-callback', [PaymentsController::class, 'callback']);
 Route::get('/get-allOrders', [OrdersController::class, 'getAllOrders'])->middleware('auth:sanctum');
-Route::post('/updateAvatar', [AuthController::class, 'updateAvatar'])->middleware('auth:sanctum');
+Route::post('/updateAvatar/{id}', [AuthController::class, 'updateAvatar'])->middleware('auth:sanctum');
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
